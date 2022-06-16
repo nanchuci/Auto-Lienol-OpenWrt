42678cf05bb52390a2cfbf3e45d20b431dd19425
nanchuci <71806013+nanchuci@users.noreply.github.com>
2022-06-16 15:07:24 +0800

    update

--- script/diy-part2.sh
+++ script/diy-part2.sh
@@ -31,7 +31,7 @@ sed -i 's/OpenWrt_5G/RAISECOM-MSG1500-5G-$/g' package/lean/mt/drivers/mt_wifi/fi
 sed -i 's/OpenWRT-5G/RAISECOM-MSG1500-5G/g' package/lean/mt/drivers/mt7615d/files/lib/wifi/mt_dbdc.sh
 
 # 添加个性信息
-sed -i 's/22.5.5/22.5.5 by nanchuci/g' ./package/lean/default-settings/files/zzz-default-settings
+sed -i 's/22.6.16/22.6.16 by nanchuci/g' ./package/lean/default-settings/files/zzz-default-settings
 
 # 修改banne文件（banne文件在根目录）（不要修改此行代码,怕弄的diy-lede.sh文件全失效,不需要的话前面加#，或者全行代码删除了）
 #rm -rf ./package/base-files/files/etc/banne && cd .. && cp -f ./banner openwrt/package/base-files/files/etc/ && cd openwrt
